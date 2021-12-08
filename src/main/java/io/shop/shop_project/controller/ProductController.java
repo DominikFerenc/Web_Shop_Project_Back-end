e102c2cb93da777c136039edfc11618da8f0464a
dferenc <dominikferenc1995@gmail.com>
2021-12-08 14:02:11 +0100

    h2 support in application.yaml add

--- src/main/java/io/shop/shop_project/controller/ProductController.java
+++ src/main/java/io/shop/shop_project/controller/ProductController.java
@@ -1,7 +1,6 @@
 package io.shop.shop_project.controller;
 
 import io.shop.shop_project.model.Product;
-import io.shop.shop_project.repository.ProductRepository;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +9,6 @@ import java.util.List;
 
 @RequestMapping("/api/products")
 public interface ProductController {
-
 
 
     @ApiOperation(value = "Getting products from db")
